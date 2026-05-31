b228bc313614fd011ed5284bec420c5751682bcf
Vishnu R <vishnu6364748848@gmail.com>
2026-05-31 11:29:02 +0530

    refactor: Optimising product.model.js

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -5,20 +5,47 @@ const productSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
     description: {
       type: String,
+      required: true,
     },
     price: {
       type: Number,
       required: true,
+      min: [0, "Price cannot be negative"],
     },
     category: {
       type: String,
+      required: true,
+      enum: [
+        "Electronics",
+        "Clothing",
+        "Books",
+        "Home",
+        "Beauty",
+        "Sports",
+        "Toys",
+        "Other",
+      ],
     },
-    imgUrl: {
-      type: String,
-    },
+    images: [
+      {
+        url: {
+          type: String,
+          required: true,
+        },
+        publicId: {
+          type: String,
+          required: true,
+        },
+        isPrimary: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
   },
   { timestamps: true },
 );

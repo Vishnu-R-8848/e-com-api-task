32287f8d3582335522455fd53b98a30b863533b2
Vishnu R <vishnu6364748848@gmail.com>
2026-05-31 15:32:07 +0530

    refactor: re-optimissing the db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected successfully...");
   } catch (error) {
     console.error(`Error in MongoDB connection: ${error.message}`);

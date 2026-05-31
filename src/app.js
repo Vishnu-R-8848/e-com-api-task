8dfec13ede8c50fd996af0968e502d0629044357
Vishnu R <vishnu6364748848@gmail.com>
2026-05-31 11:52:36 +0530

    refactor: optimissing app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-import express from 'express';
-import cookies from 'cookie-parser';
+import express from "express";
+import cookies from "cookie-parser";
+import productRoutes from "./routes/product.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cookies());
 
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
-app.use("/api/products", (await import("./routes/product.routes.js")).default);
-app.use("/api/auth", (await import("./routes/auth.routes.js")).default);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 export default app;

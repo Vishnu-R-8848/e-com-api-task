3db9096cd5986eb0fea8da78aa6a47c934fce1dd
Vishnu R <vishnu6364748848@gmail.com>
2026-05-31 11:19:07 +0530

    feat: add product.model.js in models

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,1 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+    },
+    imgUrl: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const ProductModel = mongoose.model("Product", productSchema);
+
+export default ProductModel;
